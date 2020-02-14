5808695cd2dab32e605bae4c8e88f457e73cb807
tetsu5555 <tetete1118@gmail.com>
2020-02-15 03:57:30 +0900

    init

---
+++ statement.js
@@ -0,0 +1,40 @@
+function statement (invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `Statement for ${invoice.customer}\n`;
+
+  const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
+
+  for (let perf of invoice.perfomances) {
+    const play = plays(perf.playID);
+    let thisAmount = 0
+
+    switch(play.type) {
+    case "tragedy":
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        this.thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy":
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 300 * perf.audience;
+        break;
+      }
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+    }
+
+    // ボリューム特典のポイントを加算
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // 喜劇のときは10人につき、さらにポイントを加算
+    if ('comedy' === play) volumeCredits += Math.floor(perf.audience / 5);
+    // 注文の内訳を出力
+    result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+    thisAmount += thisAmount;
+  }
+  result += `Amount owed is ${format(thisAmount/100)}\n`;
+  result += `You earned ${volumeCredits} credits\n`;
+  return result;
+}
