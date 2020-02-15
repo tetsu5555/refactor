a739054c7a3476a3e70cd216c03f10cbbe668924
tetsu5555 <tetete1118@gmail.com>
2020-02-16 08:59:38 +0900

    refactor: extract basePrice as local variable

--- src/inlining_variable/index.js
+++ src/inlining_variable/index.js
@@ -1,5 +1,6 @@
 module.exports = function price(order) {
-  return order.quantity * order.itemPrice -
+  const basePrice = order.quantity * order.itemPrice
+  return basePrice -
     Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
     Math.min(order.quantity * order.itemPrice * 0.1, 100)
 }
