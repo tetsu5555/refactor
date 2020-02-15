ec395330d2f22049034a9626ce5c331096dd4a25
tetsu5555 <tetete1118@gmail.com>
2020-02-15 17:15:51 +0900

    fix lint. semi colon never

--- src/original.js
+++ src/original.js
@@ -1,41 +1,41 @@
 module.exports = function statement (invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
+  let totalAmount = 0
+  let volumeCredits = 0
+  let result = `Statement for ${invoice.customer}\n`
 
-  const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
+  const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = 0;
+    const play = plays[perf.playID]
+    let thisAmount = 0
 
     switch(play.type) {
     case "tragedy":
-      thisAmount = 40000;
+      thisAmount = 40000
       if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
+        thisAmount += 1000 * (perf.audience - 30)
       }
-      break;
+      break
     case "comedy":
-      thisAmount = 30000;
+      thisAmount = 30000
       if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
+        thisAmount += 10000 + 500 * (perf.audience - 20)
       }
-      thisAmount += 300 * perf.audience;
-      break;
+      thisAmount += 300 * perf.audience
+      break
     default:
-      throw new Error(`unknown type: ${play.type}`);
+      throw new Error(`unknown type: ${play.type}`)
     }
 
     // ボリューム特典のポイントを加算
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    volumeCredits += Math.max(perf.audience - 30, 0)
     // 喜劇のときは10人につき、さらにポイントを加算
-    if ('comedy' === play) volumeCredits += Math.floor(perf.audience / 5);
+    if ('comedy' === play) volumeCredits += Math.floor(perf.audience / 5)
     // 注文の内訳を出力
-    result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
+    result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`
+    totalAmount += thisAmount
   }
-  result += `Amount owed is ${format(totalAmount/100)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
-  return result;
-};
+  result += `Amount owed is ${format(totalAmount/100)}\n`
+  result += `You earned ${volumeCredits} credits\n`
+  return result
+}
--- src/statement.js
+++ src/statement.js
@@ -1,48 +1,48 @@
 module.exports = function statement (invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
+  let totalAmount = 0
+  let volumeCredits = 0
+  let result = `Statement for ${invoice.customer}\n`
 
-  const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
+  const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format
 
   for (let perf of invoice.performances) {
-    const thisAmount = amounFor(playFor(perf), perf);
+    const thisAmount = amounFor(playFor(perf), perf)
 
     // ボリューム特典のポイントを加算
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    volumeCredits += Math.max(perf.audience - 30, 0)
     // 喜劇のときは10人につき、さらにポイントを加算
-    if ('comedy' === playFor(perf)) volumeCredits += Math.floor(perf.audience / 5);
+    if ('comedy' === playFor(perf)) volumeCredits += Math.floor(perf.audience / 5)
     // 注文の内訳を出力
-    result += ` ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
+    result += ` ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`
+    totalAmount += thisAmount
   }
-  result += `Amount owed is ${format(totalAmount/100)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
-  return result;
+  result += `Amount owed is ${format(totalAmount/100)}\n`
+  result += `You earned ${volumeCredits} credits\n`
+  return result
 
   function amounFor(play, aPerformance) {
-    let result = 0;
+    let result = 0
     switch(play.type) {
     case "tragedy":
-      result = 40000;
+      result = 40000
       if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
+        result += 1000 * (aPerformance.audience - 30)
       }
-      break;
+      break
     case "comedy":
-      result = 30000;
+      result = 30000
       if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
+        result += 10000 + 500 * (aPerformance.audience - 20)
       }
-      result += 300 * aPerformance.audience;
-      break;
+      result += 300 * aPerformance.audience
+      break
     default:
-      throw new Error(`unknown type: ${play.type}`);
+      throw new Error(`unknown type: ${play.type}`)
     }
-    return result;
+    return result
   }
 
   function playFor(aPerformance) {
-    return plays[aPerformance.playID];
+    return plays[aPerformance.playID]
   }
-};
+}
