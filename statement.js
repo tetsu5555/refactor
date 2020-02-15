b31d30e3f26eb7c8ed26428b00455931879213c8
tetsu5555 <tetete1118@gmail.com>
2020-02-15 16:38:43 +0900

    extract amountFor func

--- statement.js
+++ statement.js
@@ -1,30 +1,14 @@
-function statement (invoice, plays) {
+module.exports = function statement (invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
 
-  for (let perf of invoice.perfomances) {
-    const play = plays(perf.playID);
-    let thisAmount = 0
-
-    switch(play.type) {
-    case "tragedy":
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        this.thisAmount += 1000 * (perf.audience - 30);
-      }
-      break;
-    case "comedy":
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 300 * perf.audience;
-        break;
-      }
-    default:
-      throw new Error(`unknown type: ${play.type}`);
-    }
+  for (let perf of invoice.performances) {
+    console.log(perf)
+    const play = plays[perf.playID];
+    const thisAmount = amounFor(play, perf);
 
     // ボリューム特典のポイントを加算
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -33,9 +17,30 @@ function statement (invoice, plays) {
     // 注文の内訳を出力
     result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
-    return result;
   }
   result += `Amount owed is ${format(totalAmount/100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
+
+  function amounFor(play, perf) {
+    let thisAmount = 0
+    switch(play.type) {
+    case "tragedy":
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy":
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+    }
+    return thisAmount;
+  }
 }
