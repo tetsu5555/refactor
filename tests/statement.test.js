0772f41548fa839411b2f70f82a198b3cca0422b
tetsu5555 <tetete1118@gmail.com>
2020-02-15 23:02:41 +0900

    test for statement.js

---
+++ tests/statement.test.js
@@ -0,0 +1,40 @@
+const statement = require('../src/statement')
+
+
+
+test('Statement', () => {
+  const invoice = {
+    "customer": "BigCo",
+    "performances": [
+      {
+        "playID": "hamlet",
+        "audience": 55
+      },
+      {
+        "playID": "as-like",
+        "audience": 35
+      },
+      {
+        "playID": "othello",
+        "audience": 40
+      }
+    ]
+  }
+
+  const plays = {
+    "hamlet": {"name": "Hamlet", "type": "tragedy"},
+    "as-like": {"name": "As You Like It", "type": "comedy"},
+    "othello": {"name": "Othello", "type": "tragedy"}
+  }
+
+  const result = `Statement for BigCo
+ Hamlet: $650.00 (55 seats)
+ As You Like It: $580.00 (35 seats)
+ Othello: $500.00 (40 seats)
+Amount owed is $1,730.00
+You earned 40 credits
+`
+
+
+  expect(statement(invoice, plays)).toBe(result)
+})
