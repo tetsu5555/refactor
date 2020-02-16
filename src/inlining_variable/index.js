e82eda655a45f13b54a03eb6e80582b161c51729
tetsu5555 <tetete1118@gmail.com>
2020-02-16 09:03:29 +0900

    refactor: extract variable shipping

--- src/inlining_variable/index.js
+++ src/inlining_variable/index.js
@@ -1,7 +1,6 @@
 module.exports = function price(order) {
   const basePrice = order.quantity * order.itemPrice
   const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05
-  return basePrice -
-    quantityDiscount +
-    Math.min(order.quantity * order.itemPrice * 0.1, 100)
+  const shipping = Math.min(order.quantity * order.itemPrice * 0.1, 100)
+  return basePrice - quantityDiscount + shipping
 }
