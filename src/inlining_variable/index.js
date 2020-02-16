dc7b26652768004359d131fc21c6b8f06ee2b77e
tetsu5555 <tetete1118@gmail.com>
2020-02-16 09:02:09 +0900

    refactor: extract quantityDiscount as local variable

--- src/inlining_variable/index.js
+++ src/inlining_variable/index.js
@@ -1,6 +1,7 @@
 module.exports = function price(order) {
   const basePrice = order.quantity * order.itemPrice
+  const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05
   return basePrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+    quantityDiscount +
     Math.min(order.quantity * order.itemPrice * 0.1, 100)
 }
