8463bddcf87a50dc78e807c9281c3918d270cdd7
tetsu5555 <tetete1118@gmail.com>
2020-02-16 09:25:03 +0900

    refactor: extract as methods

--- src/extract_variable/index_class.js
+++ src/extract_variable/index_class.js
@@ -7,8 +7,11 @@ module.exports = class Order {
   get itemPrice() { return this._data.itemPrice }
 
   get price() {
-    return this.quantity * this.itemPrice -
-      Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
-      Math.min(this.quantity * this.itemPrice * 0.1, 100)
+    return this.basePrice - this.quantityDiscount + this.shipping
   }
+  // 名前を変数ではなく、メソッドとして抽出する
+  // オブジェクトに与えられた適切な大きさのコンテキストを通じて他のロジックやデータを共有できる
+  get basePrice() { return this.quantity * this.itemPrice }
+  get quantityDiscount() { return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 }
+  get shipping() { return Math.min(this.quantity * this.itemPrice * 0.1, 100) }
 }
