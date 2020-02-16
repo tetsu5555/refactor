2587504f4d4fea38efe95c6a77bca5ebf7b33dee
tetsu5555 <tetete1118@gmail.com>
2020-02-16 14:50:36 +0900

    refactor by extracting functions

--- src/extract_function/index.js
+++ src/extract_function/index.js
@@ -1,20 +1,33 @@
 const printOwing = function(invoice) {
-  let outstanding = 0
+  printBanner()
+  const outstanding = calculateOutstanding()
+  recordDueDate(invoice)
+  printDetails(invoice, outstanding)
 
-  console.log("************************\n")
-  console.log("**** Customer Owes ****\n")
-  console.log("***********************\n")
-
-  for (const o of invoice.orders) {
-    outstanding += o.amount
+  function printBanner() {
+    console.log("************************\n")
+    console.log("**** Customer Owes ****\n")
+    console.log("***********************\n")
   }
 
-  const today = new Date()
-  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30)
+  function recordDueDate(invoice) {
+    const today = new Date()
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30)
+  }
 
-  console.log(`name: ${invoice.customer}\n`)
-  console.log(`amount: ${outstanding}\n`)
-  console.log(`due: ${invoice.dueDate.toLocaleDateString()}`)
+  function calculateOutstanding() {
+    let result = 0
+    for (const o of invoice.orders) {
+      result += o.amount
+    }
+    return result
+  }
+
+  function printDetails(invoice, outstanding) {
+    console.log(`name: ${invoice.customer}\n`)
+    console.log(`amount: ${outstanding}\n`)
+    console.log(`due: ${invoice.dueDate.toLocaleDateString()}`)
+  }
 }
 
 module.exports = { printOwing }
