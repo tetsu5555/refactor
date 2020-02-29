3beb55da1ed17410b4c54798562e4ff9c23577c1
tetsu5555 <tetete1118@gmail.com>
2020-02-29 18:32:00 +0900

    rename zztmp → emitPhotoData

--- src/moveStatementToCaller/index.js
+++ src/moveStatementToCaller/index.js
@@ -2,7 +2,7 @@ function renderPerson(outStream, person) {
   outStream.write(`<p>${person.name}</p>\n`)
   renderPhoto(outStream, person.renderPhoto)
   // 変数のインライン化を行っていく
-  zztmp(outStream, person.photo)
+  emitPhotoData(outStream, person.photo)
   outStream.write(`<p>location: ${person.photo.location}</p>\n`)
 }
 
@@ -12,20 +12,14 @@ function listRecentPhotos(outStream, photos) {
     .forEach(p => {
       outStream.write("<div>\n")
       // 変数のインライン化を行っていく
-      zztmp(outStream, p)
+      emitPhotoData(outStream, p)
       outStream.write(`<p>location: ${p.location}</p>\n`)
       outStream.write("</div>\n")
     })
 }
 
-// インライン化を行ったことでこの関数は使われなくなった
-function emitPhotoData(outStream, photo) {
-  zztmp(outStream, photo)
-  outStream.write(`<p>location: ${photo.location}</p>\n`)
-}
-
 // 簡単にgrepできるものしておくと良い
-function zztmp(outStream, photo) {
+function emitPhotoData(outStream, photo) {
   outStream.write(`<p>title: ${photo.title}</p>\n`)
   outStream.write(`<p?date: ${photo.date.toDateString()}</p>\n`)
 }
