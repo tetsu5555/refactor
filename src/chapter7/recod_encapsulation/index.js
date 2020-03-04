4aee4c989bc6df8bf838ad4f4337bfc3bebff5b0
tetsu5555 <tetete1118@gmail.com>
2020-03-05 00:29:01 +0900

    update

--- src/chapter7/recod_encapsulation/index.js
+++ src/chapter7/recod_encapsulation/index.js
@@ -2,21 +2,25 @@ class Organization {
   constructor(data) {
     this._data = data
   }
+
+  // setterを生やす
+  set name(aString) { this._data.name = aString }
+  // getterを生やす
+  get name() { return this._data.name }
 }
 
 const organization = new Organization({ name: "Acme Gooseberries", country: "GB" })
 
-// オブジェクトを返す関数を定義
-function getRawDataOrganization() { return organization._data }
+// オブジェクトを返す関数を定義、これにgetetr,setterを生やして処理を置き換える
 function getOrganization() { return organization }
 
 function main() {
   let result = ""
-  result += `<h1>${getRawDataOrganization().name}</h1>\n`
+  result += `<h1>${getOrganization().name}</h1>\n`
 
-  getRawDataOrganization().name = "Hoge"
+  getOrganization().name = "Hoge"
 
-  result += `<h1>${getRawDataOrganization().name}</h1>`
+  result += `<h1>${getOrganization().name}</h1>`
 
   console.log(result)
 }
