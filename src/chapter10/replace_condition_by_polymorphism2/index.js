ccfb086c605c077f845eae1c3c7dda9a90aeb8e4
tetsu5555 <tetete1118@gmail.com>
2020-03-06 00:21:35 +0900

    aggregate functions to class

--- src/chapter10/replace_condition_by_polymorphism2/index.js
+++ src/chapter10/replace_condition_by_polymorphism2/index.js
@@ -1,45 +1,58 @@
 function rating(voyage, history) {
-  const vpf = voyageProfileFactor(voyage, history)
-  const vr = voyageRisk(voyage)
-  const chr = captainHistoryRisk(voyage, history)
-  if (vpf * 3 > (vr + chr * 2)) return "A"
-  else return "B"
+  return new Rating(voyage, history).value
 }
 
-function voyageRisk(voyage) {
-  let result = 1
-  if (voyage.length > 4) result += 2
-  if (voyage.length > 8) result += voyage.length - 8
-  if (["china", "east-indies"].includes(voyage.zone)) result += 4
-  return Math.max(result, 0)
-}
-
-function captainHistoryRisk(voyage, history) {
-  let result = 1
-  if (history.length < 5) result += 4
-  result += history.filter(v => v.profit < 0)
-  if (voyage.zone === "china" && hasChina(history)) result += 2
-  return Math.max(result, 2)
-}
-
-function hasChina(history) {
-  return history.some(v => "china" === v.zone)
-}
-
-function voyageProfileFactor(voyage, history) {
-  let result = 2
-  if (voyage.zone === "china") result += 1
-  if (voyage.zone === "east-indies") result += 1
-  if (voyage.zone === "china" && hasChina(history)) {
-    result += 3
-    if (history.length > 10) result += 1
-    if (voyage.length > 12) result += 1
-    if (voyage.length > 118) result -= 1
-  } else {
-    if (history.length > 8) result += 1
-    if (voyage.length > 14) result -= 1
+// ポリモーフィズムを導入するにはクラス構造を作成する必要があるため
+// 「関数軍のクラスへの集約」を行う
+class Rating {
+  constructor(voyage, history) {
+    this.voyage = voyage
+    this.history = history
   }
-  return result
+
+  get value() {
+    const vpf = this.voyageProfileFactor
+    const vr = this.voyageRisk
+    const chr = this.captainHistoryRisk
+    if (vpf * 3 > (vr + chr * 2)) return "A"
+    else return "B"
+  }
+
+  get voyageRisk() {
+    let result = 1
+    if (this.voyage.length > 4) result += 2
+    if (this.voyage.length > 8) result += this.voyage.length - 8
+    if (["china", "east-indies"].includes(this.voyage.zone)) result += 4
+    return Math.max(result, 0)
+  }
+
+  get captainHistoryRisk() {
+    let result = 1
+    if (this.history.length < 5) result += 4
+    result += this.history.filter(v => v.profit < 0)
+    if (this.voyage.zone === "china" && this.hasChinaHistory) result += 2
+    return Math.max(result, 2)
+  }
+
+  get hasChinaHistory() {
+    return this.history.some(v => "china" === v.zone)
+  }
+
+  get voyageProfileFactor() {
+    let result = 2
+    if (this.voyage.zone === "china") result += 1
+    if (this.voyage.zone === "east-indies") result += 1
+    if (this.voyage.zone === "china" && this.hasChinaHistory) {
+      result += 3
+      if (this.history.length > 10) result += 1
+      if (this.voyage.length > 12) result += 1
+      if (this.voyage.length > 118) result -= 1
+    } else {
+      if (this.history.length > 8) result += 1
+      if (this.voyage.length > 14) result -= 1
+    }
+    return result
+  }
 }
 
 function main() {
