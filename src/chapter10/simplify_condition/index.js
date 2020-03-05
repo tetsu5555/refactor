d4da1f444d2249df28f364a8d1f6049b8ee9616b
tetsu5555 <tetete1118@gmail.com>
2020-03-05 18:07:28 +0900

    aggregate to Bird class

--- src/chapter10/simplify_condition/index.js
+++ src/chapter10/simplify_condition/index.js
@@ -1,3 +1,35 @@
+class Bird {
+  constructor(birdObject) {
+    Object.assign(this, birdObject)
+  }
+
+  get plumage() {
+    switch(this.type) {
+    case "EuropeanSwallow":
+      return "average"
+    case "AfricanSwallow":
+      return (this.numberOfCoconuts > 2) ? "tired" : "average"
+    case "NorwegianBlueParrot":
+      return (this.voltage > 100) ? "scorched" : "beautiful"
+    default:
+        return "unknown"
+    }
+  }
+
+  get airSpeedVelocity() {
+    switch(this.type) {
+    case "EuropeanSwallow":
+      return 35
+    case "AfricanSwallow":
+      return 42 - 2 * this.numberOfCoconuts
+    case "NorwegianBlueParrot":
+      return (this.isNailed) ? 0 : 10 + this.voltage / 10
+    default:
+        return null
+    }
+  }
+}
+
 function plumages(birds) {
   return new Map(birds.map(b => [b.name, plumage(b)]))
 }
@@ -6,30 +38,14 @@ function speeds(birds) {
   return new Map(birds.map(b => [b.name, airSpeedVelocity(b)]))
 }
 
+// Birdクラスに処理を集約させた
 function plumage(bird) {
-  switch(bird.type) {
-  case "EuropeanSwallow":
-    return "average"
-  case "AfricanSwallow":
-    return (bird.numberOfCoconuts > 2) ? "tired" : "average"
-  case "NorwegianBlueParrot":
-    return (bird.voltage > 100) ? "scorched" : "beautiful"
-  default:
-      return "unknown"
-  }
+  return new Bird(bird).plumage
 }
 
+// Birdクラスに処理を集約させた
 function airSpeedVelocity(bird) {
-  switch(bird.type) {
-  case "EuropeanSwallow":
-    return 35
-  case "AfricanSwallow":
-    return 42 - 2 * bird.numberOfCoconuts
-  case "NorwegianBlueParrot":
-    return (bird.isNailed) ? 0 : 10 + bird.voltage / 10
-  default:
-      return null
-  }
+  return new Bird(bird).airSpeedVelocity
 }
 
 function main() {
