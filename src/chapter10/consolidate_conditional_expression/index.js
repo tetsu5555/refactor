3dafc8c93c754ab173a96e2b85645569cdf37919
tetsu5555 <tetete1118@gmail.com>
2020-03-07 17:59:30 +0900

    consolidate

--- src/chapter10/consolidate_conditional_expression/index.js
+++ src/chapter10/consolidate_conditional_expression/index.js
@@ -1,6 +1,9 @@
+// 条件記述の統合
+
 function disabilityAmount(aEmployee) {
-  if (aEmployee.seniority < 2) return 0
-  if (aEmployee.monthsDisabled > 12) return 0
-  if (aEmployee.isPartTime) return 0
+  // 結果は全て同じため、条件を一つにまとめる
+  if (aEmployee.seniority < 2
+    || aEmployee.monthsDisabled > 12
+    || aEmployee.isPartTime) return 0
   // 障害手当を計算する
 }
