d33c0586d9672d746acbaed27404e0561b4446fc
tetsu5555 <tetete1118@gmail.com>
2020-03-07 18:12:36 +0900

    update

--- src/chapter10/consolidate_conditional_expression/index.js
+++ src/chapter10/consolidate_conditional_expression/index.js
@@ -4,14 +4,8 @@ function disabilityAmount(anEmployee) {
   // 結果は全て同じため、条件を一つにまとめる
   if (isNotEligileForDisability()) return 0
 
-  if (anEmployee.onVacation) {
-    if (anEmployee.sno > 10) {
-      return 0
-    }
-    return 0.5
-  }
-  // 障害手当を計算する
-  // 処理......
+  if (anEmployee.onVacation && anEmployee.seniority > 10) return 0
+  return 0.5
 
   // 関数として抜き出す
   function isNotEligileForDisability() {
