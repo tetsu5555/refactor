496ed8a78e7bb59b473cda4605fd4366c4c451ab
tetsu5555 <tetete1118@gmail.com>
2020-03-07 20:39:29 +0900

    remove flag argument

---
+++ src/chapter11/remove_flag_argument/original.js
@@ -0,0 +1,34 @@
+function setDimension(name, value) {
+  if (name === "height") {
+    this._height = value
+    return
+  }
+  if (name === "width") {
+    this._width = value
+    return
+  }
+}
+
+function deliveryDate(anOder, isRush) {
+  if (isRush) {
+    let deliveryTime
+    if (["MA", "CT"].includes(anOder.deliveryState)) {
+      deliveryTime = 1
+    } else if (["NY", "NH"].includes(anOder.deliveryState)) {
+      deliveryTime = 2
+    } else {
+      deliveryTime = 3
+    }
+    return anOder.placedOn.plusDays(1 + deliveryTime)
+  } else {
+    let deliveryTime
+    if (["MA", "CT", "NY"].includes(anOder.deliveryState)) {
+      deliveryTime = 2
+    } else if (["ME", "NH"].includes(anOder.deliveryState)) {
+      deliveryTime = 3
+    } else {
+      deliveryTime = 4
+    }
+    return anOder.placedOn.plusDays(2 + deliveryTime)
+  }
+}
