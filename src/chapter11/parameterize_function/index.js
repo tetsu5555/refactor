abd1e714d1ab7e679d6daf2a61ae1e69fa4f0aae
tetsu5555 <tetete1118@gmail.com>
2020-03-07 19:59:35 +0900

    replace middleBand with withBand

--- src/chapter11/parameterize_function/index.js
+++ src/chapter11/parameterize_function/index.js
@@ -1,15 +1,21 @@
 function baseCharge(usage) {
   if (usage < 0) return 0
   const amount = bottomBand(usage) * 0.03
-                + middleBand(usage) * 0.05
+                + withinBand(usage, 200, 100) * 0.05
                 + topBand(usage) * 0.07
   return amount
+}
+
+function withinBand(usage, bottom, top) {
+  return usage > 100 ? Math.min(usage, 200) - 100 : 0
 }
 
 function bottomBand(usage) {
   return Math.min(usage, 100)
 }
 
+// 関連する複数の関数をパラメータ付きにしようとする時は関数を一つ取り上げてそれにパラメータを追加する
+// 範囲に基づくロジックでは通常中間の値から手を付けるらしい
 function middleBand(usage) {
   return usage > 100 ? Math.min(usage, 200) - 100 : 0
 }
