033921792ff6ad457564a4cbb22e95f0cdc0190b
tetsu5555 <tetete1118@gmail.com>
2020-03-07 18:09:40 +0900

    fix

--- src/chapter10/consolidate_conditional_expression/index.js
+++ src/chapter10/consolidate_conditional_expression/index.js
@@ -1,9 +1,22 @@
 // 条件記述の統合
 
-function disabilityAmount(aEmployee) {
+function disabilityAmount(anEmployee) {
   // 結果は全て同じため、条件を一つにまとめる
-  if (aEmployee.seniority < 2
-    || aEmployee.monthsDisabled > 12
-    || aEmployee.isPartTime) return 0
+  if (isNotEligileForDisability()) return 0
+
+  if (anEmployee.onVacation) {
+    if (anEmployee.sno > 10) {
+      return 0
+    }
+    return 0.5
+  }
   // 障害手当を計算する
+  // 処理......
+
+  // 関数として抜き出す
+  function isNotEligileForDisability() {
+    return (anEmployee.seniority < 2
+            || anEmployee.monthsDisabled > 12
+            || anEmployee.isPartTime)
+  }
 }
--- src/chapter10/consolidate_conditional_expression/original.js
+++ src/chapter10/consolidate_conditional_expression/original.js
@@ -1,6 +1,13 @@
-function disabilityAmount(aEmployee) {
-  if (aEmployee.seniority < 2) return 0
-  if (aEmployee.monthsDisabled > 12) return 0
-  if (aEmployee.isPartTime) return 0
+function disabilityAmount(anEmployee) {
+  if (anEmployee.seniority < 2) return 0
+  if (anEmployee.monthsDisabled > 12) return 0
+  if (anEmployee.isPartTime) return 0
+
+  if (anEmployee.onVacation) {
+    if (anEmployee.sno > 10) {
+      return 0
+    }
+    return 0.5
+  }
   // 障害手当を計算する
 }
