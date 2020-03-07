3767ea1f4ffcf4ed65695fdd4f2d858d4a0eb8a9
tetsu5555 <tetete1118@gmail.com>
2020-03-07 18:28:12 +0900

    add

--- src/chapter10/replace_nested_conditional_with_guard_clauses/index.js
+++ src/chapter10/replace_nested_conditional_with_guard_clauses/index.js
@@ -7,3 +7,13 @@ function payAmount(employee) {
   ut.enim.ad(minim.veniam)
   return someFinalComputation()
 }
+
+function adjustedCapital(anInstrument) {
+  let result = 0
+  if (anInstrument.capital > 0) {
+    if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
+      result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor
+    }
+  }
+  return result
+}
